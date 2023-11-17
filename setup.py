8379dae7db88d59bfa2d30407cf6e193395c38da
Guanghui Qin <hiaoxui@gmail.com>
2023-11-17 15:04:26 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='nugget',
+    version='0.1.0',
+    packages=[ 'nugget', 'nugget.adaptors', 'nugget.utils'],
+    url='https://github.com/hiaoxui/nugget',
+    license='MIT',
+    author='Guanghui Qin',
+    author_email='hiaoxui@gmail.com',
+    description='Nugget text representation',
+)
